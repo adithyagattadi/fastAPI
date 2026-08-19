4bfa4e00d2d7671ba778c7e363a64137b867cd50
adithyagattadi <adithyagattadi123@gmail.com>
2026-08-19 15:29:58 +0530

    Status Codes + Custom Responses + Error Handling

--- main.py
+++ main.py
@@ -1,27 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 from pydantic import BaseModel
+
 
 app = FastAPI()
 
-{
-    "name": "Adi",
-    "age": 23,
-    "Password": 12345
-}
 
 class User(BaseModel):
-    name:str
-    age:int
-    password:str
+    name: str
+    age: int
 
-class userResponse(BaseModel):
-    name:str
-    age:int
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    age: int
 
-@app.get("/user", response_model=userResponse)
+
+@app.post("/create_user", status_code=status.HTTP_201_CREATED)
+def create_user(user: User):
+    return {
+        "message": "User Created",
+        "data": user
+    }
+
+
+@app.get("/user", response_model=UserResponse)
 def get_user():
-    return{
-        "name":"Adi",
-        "age":23,
-        "password":12345
+    return {
+        "id": 1,
+        "name": "Adi",
+        "age": 23
+    }
+
+
+@app.get("/users/{user_id}")
+def get_user_by_id(user_id: int):
+    if user_id != 1:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+    return {
+        "id": 1,
+        "name": "Adi",
+        "age": 23
     }
