16370c9305a1da0bdf90499df9e8ddd2a8d327f5
adithyagattadi <adithyagattadi123@gmail.com>
2026-08-20 22:42:01 +0530

    Auth Example

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Header, HTTPException
 
 app = FastAPI()
+
+def verify_token(token: str = Header(None)):
+    if token != "my_secret_token":
+        raise HTTPException(
+            status_code=404,
+            detail= "Unauthorized"
+        )
+    return{
+        "user": "Authorized User"
+    }
+
+@app.get("/secure-data")
+def secure_data(user=Depends(verify_token)):
+    return{
+        "message": "secure data access",
+        "user":user 
+    }
 
 # def common_logic():
 #     return{
@@ -10,15 +27,16 @@ app = FastAPI()
 # def home(data=Depends(common_logic)):
 #     return data
 
-def get_current_user():
-    return {
-        "user": "mohit"
-    }
+# def get_current_user():
+#     return {
+#         "user": "mohit"
+#     }
 
-@app.get("/profile")
-def profile(user=Depends(get_current_user)):
-    return user
+# @app.get("/profile")
+# def profile(user=Depends(get_current_user)):
+#     return user
 
-@app.get("/dashboard")
-def profile(user=Depends(get_current_user)):
-    return user
+# @app.get("/dashboard")
+# def profile(user=Depends(get_current_user)):
+#     return user
+
