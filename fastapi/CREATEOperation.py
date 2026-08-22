ce98ee2792a5270c1e1f5d53ea2fb790bd71a645
adithyagattadi <adithyagattadi123@gmail.com>
2026-08-22 17:03:19 +0530

    CREATE Operation with Database

---
+++ fastapi/CREATEOperation.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session, declarative_base, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String
+
+app = FastAPI()
+
+DATABASE_URL = "sqlite:///./test.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+session_local = sessionmaker(bind=engine)
+Base = declarative_base()
+
+class Todo(Base):
+    __tablename__ = "todos"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    completed = Column(Integer, default=0)
+
+Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = session_local()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Create API
+@app.post("/todos/")
+def create_todo(title:str, db: Session = Depends(get_db)):
+    todo = Todo(title=title, completed="False")
+    db.add(todo)
+    db.commit()
+    db.refresh(todo)
+    return{
+        "message": "Todo created successfully",
+        "data": todo
+    }
