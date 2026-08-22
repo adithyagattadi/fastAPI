1d71b6ba1eefd0f3a8296d8d42b76193708dddf2
adithyagattadi <adithyagattadi123@gmail.com>
2026-08-22 13:59:32 +0530

    SQLite Database Integration + Setup + SQLAlchemy

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-from fastapi import FastAPI, Request
-import time
+import sqlite3
+from fastapi import FastAPI
 
 app = FastAPI()
 
-# @app.middleware("http")
-# async def my_middleware(request: Request, call_next):
-#     print("Request Received")
-#     response = await call_next(Request)
-#     print("Response Sent")
-#     return response
+conn = sqlite3.connect("test.db", check_same_thread=False)
 
-@app.middleware("http")
-async def log_middleware(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(Request)
-    process_time = time.time() - start_time
-    print(f"Path:{request.url.path} | Time: {process_time}")
-    return response
+cursor = conn.cursor()
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS todos(
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        completed TEXT
+    )
+""")
+
+conn.commit()
+
+@app.get("/home")
+def home():
+    return{
+        "message": "SQLlite connected fine"
+    }
